892a4799465682d86dbdff30523c8dbb4c03ecef
Inoxia25 <nandinisapna1401@gmail.com>
2020-09-26 12:22:48 +0530

    Added process.env.PORT

--- app.js
+++ app.js
@@ -93,6 +93,6 @@ app.delete("/blogs/:id",function(req,res){
 
 
 
-app.listen(3000, function(){ 
+app.listen(process.env.PORT ||3000, function(){ 
   console.log('Server listening on port 3000');
 });
